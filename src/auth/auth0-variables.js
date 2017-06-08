8ca2287311c9b525dcc6f546bfb35b2c4f61d0df
joshuajohnson <krauselwitz@me.com>
2017-06-08 14:55:33 -0500

    initial

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   clientId: 'yFWOVmekQFILrLsPRl5D2Mb5BW6fjZO4',
   domain: 'joshua-johnson.auth0.com',
-  callbackUrl: 'http://localhost:8080/callback',
+  callbackUrl: 'hhttp://ec2-52-25-23-217.us-west-2.compute.amazonaws.com:8080/callback',
   apiUrl: '{https://joshua-johnson.auth0.com/api/v2/}'
 }
